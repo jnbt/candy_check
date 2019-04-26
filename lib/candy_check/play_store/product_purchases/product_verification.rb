550e61a99de34428043feb52318b68804a795277
Christoph Weegen <cw@neopoly.de>
2019-04-26 12:18:19 +0200

    Correct namespaces of moved files

--- lib/candy_check/play_store/product_purchases/product_verification.rb
+++ lib/candy_check/play_store/product_purchases/product_verification.rb
@@ -1,47 +1,49 @@
 module CandyCheck
   module PlayStore
-    # Verifies a purchase token against the Google API
-    # The call return either an {Receipt} or an {VerificationFailure}
-    class ProductVerification
-      # @return [String] the package which will be queried
-      attr_reader :package
-      # @return [String] the item id which will be queried
-      attr_reader :product_id
-      # @return [String] the token for authentication
-      attr_reader :token
+    module ProductPurchases
+      # Verifies a purchase token against the Google API
+      # The call return either an {Receipt} or an {VerificationFailure}
+      class ProductVerification
+        # @return [String] the package which will be queried
+        attr_reader :package
+        # @return [String] the item id which will be queried
+        attr_reader :product_id
+        # @return [String] the token for authentication
+        attr_reader :token
 
-      # Initializes a new call to the API
-      # @param client [Client] a shared client instance
-      # @param package [String]
-      # @param product_id [String]
-      # @param token [String]
-      def initialize(client, package, product_id, token)
-        @client = client
-        @package = package
-        @product_id = product_id
-        @token = token
-      end
-
-      # Performs the verification against the remote server
-      # @return [Receipt] if successful
-      # @return [VerificationFailure] otherwise
-      def call!
-        verify!
-        if valid?
-          CandyCheck::PlayStore::ProductPurchase::Receipt.new(@response)
-        else
-          VerificationFailure.new(@response["error"])
+        # Initializes a new call to the API
+        # @param client [Client] a shared client instance
+        # @param package [String]
+        # @param product_id [String]
+        # @param token [String]
+        def initialize(client, package, product_id, token)
+          @client = client
+          @package = package
+          @product_id = product_id
+          @token = token
         end
-      end
 
-      private
+        # Performs the verification against the remote server
+        # @return [Receipt] if successful
+        # @return [VerificationFailure] otherwise
+        def call!
+          verify!
+          if valid?
+            CandyCheck::PlayStore::ProductPurchase::Receipt.new(@response)
+          else
+            VerificationFailure.new(@response["error"])
+          end
+        end
 
-      def valid?
-        @response && @response["purchaseState"] && @response["consumptionState"]
-      end
+        private
 
-      def verify!
-        @response = @client.verify(package, product_id, token)
+        def valid?
+          @response && @response["purchaseState"] && @response["consumptionState"]
+        end
+
+        def verify!
+          @response = @client.verify(package, product_id, token)
+        end
       end
     end
   end
