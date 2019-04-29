5eea8cdf0a55cc15ab984f8ed0adfb575f7eb597
Christoph Weegen <cw@neopoly.de>
2019-04-29 15:05:08 +0200

    Add development dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,28 @@
-require 'coveralls'
+require "coveralls"
 Coveralls.wear!
 
-require 'candy_check'
-require 'candy_check/cli'
+require "candy_check"
+require "candy_check/cli"
 
-require 'minitest/autorun'
-require 'minitest/around/spec'
+def in_continuous_integration_environment?
+  ENV["CI"] || ENV["TRAVIS"] || ENV["CONTINUOUS_INTEGRATION"]
+end
 
-require 'webmock/minitest'
+require "minitest/autorun"
+require "minitest/around/spec"
+require "minitest/focus" unless in_continuous_integration_environment?
 
-require 'timecop'
+require "webmock/minitest"
 
-require_relative 'support/with_fixtures'
-require_relative 'support/with_temp_file'
-require_relative 'support/with_command'
+require "timecop"
 
-ENV['DEBUG'] && Google::APIClient.logger.level = Logger::DEBUG
+require "pry"
+
+require_relative "support/with_fixtures"
+require_relative "support/with_temp_file"
+require_relative "support/with_command"
+
+ENV["DEBUG"] && Google::APIClient.logger.level = Logger::DEBUG
 
 module MiniTest
   module Assertions
