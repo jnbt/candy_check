c4c8a952fb41af3f5f97ad8f3bbd47bd2f3c7daa
christopheragnus <18465803+christopheragnus@users.noreply.github.com>
2021-06-25 18:19:15 +1000

    remove file open

--- lib/candy_check/play_store.rb
+++ lib/candy_check/play_store.rb
@@ -1,15 +1,15 @@
-require "google/apis/androidpublisher_v3"
+require 'google/apis/androidpublisher_v3'
 
-require "candy_check/play_store/android_publisher_service"
-require "candy_check/play_store/product_purchases/product_purchase"
-require "candy_check/play_store/subscription_purchases/subscription_purchase"
-require "candy_check/play_store/product_purchases/product_verification"
-require "candy_check/play_store/product_acknowledgements/acknowledgement"
-require "candy_check/play_store/product_acknowledgements/response"
-require "candy_check/play_store/subscription_purchases/subscription_verification"
-require "candy_check/play_store/verification_failure"
-require "candy_check/play_store/verifier"
-require "candy_check/play_store/acknowledger"
+require 'candy_check/play_store/android_publisher_service'
+require 'candy_check/play_store/product_purchases/product_purchase'
+require 'candy_check/play_store/subscription_purchases/subscription_purchase'
+require 'candy_check/play_store/product_purchases/product_verification'
+require 'candy_check/play_store/product_acknowledgements/acknowledgement'
+require 'candy_check/play_store/product_acknowledgements/response'
+require 'candy_check/play_store/subscription_purchases/subscription_verification'
+require 'candy_check/play_store/verification_failure'
+require 'candy_check/play_store/verifier'
+require 'candy_check/play_store/acknowledger'
 
 module CandyCheck
   # Module to request and verify a AppStore receipt
@@ -19,8 +19,8 @@ module CandyCheck
     # @return [Google::Auth::ServiceAccountCredentials]
     def self.authorization(json_key_file)
       Google::Auth::ServiceAccountCredentials.make_creds(
-        json_key_io: File.open(json_key_file),
-        scope: "https://www.googleapis.com/auth/androidpublisher",
+        json_key_io: json_key_file,
+        scope: 'https://www.googleapis.com/auth/androidpublisher'
       )
     end
   end
